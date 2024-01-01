2600b098bd2840d2c24a243591e01c088c2d0ade
Harsh Rawat <20bcs050@iiitdwd.ac.in>
2024-01-01 18:59:08 +0000

    operations

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -1,11 +1,11 @@
 let score =undefined
  
-console.log( typeof score);
-console.log( typeof(score));
+// console.log( typeof score);
+// console.log( typeof(score));
 
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33"=>33
 // "33abc"=>NaN
@@ -17,8 +17,26 @@ console.log(valueInNumber);
 let isLoggedIn="harsh"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=>true;0=>false;
 // ""=>false
 // "harsh"=>true;
+
+// ************** Operations****************
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" Harsh"
+
+let str3=str1+str2;
+console.log(str3);
