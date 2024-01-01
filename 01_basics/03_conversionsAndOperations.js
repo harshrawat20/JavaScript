abebae92c145a283261119d174bd5461aef125aa
Harsh Rawat <20bcs050@iiitdwd.ac.in>
2024-01-01 18:29:17 +0000

    Conversions of data types

---
+++ 01_basics/03_conversionsAndOperations.js
@@ -0,0 +1,24 @@
+let score =undefined
+ 
+console.log( typeof score);
+console.log( typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1;false=>0
+// undefined=>NaN
+// null=>0
+
+
+let isLoggedIn="harsh"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1=>true;0=>false;
+// ""=>false
+// "harsh"=>true;
